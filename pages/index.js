1e97a77e76fc54e88cafdab22de819a01dd4590d
Olvisgil <48240668+Olvisgil@users.noreply.github.com>
2022-12-08 17:30:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
+ <!--
         <ul className="w-full">
           {posts.map((post) => (
             <li
@@ -45,6 +46,11 @@ export default function Index({ posts, globalData }) {
             </li>
           ))}
         </ul>
+        -->
+    </div>
+  )
+}
+
       </main>
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground

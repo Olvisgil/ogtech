047d98a36fcf80d8149bb4620ab8ce4b41383765
Olvisgil <48240668+Olvisgil@users.noreply.github.com>
2022-12-08 17:14:48 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'OG Technologies EU';
+    : 'Joe Doe';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'We help companies to learn and integrate Web3/Blockchain technology';
+    : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';

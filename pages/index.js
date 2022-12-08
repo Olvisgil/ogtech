611cfdadaf417125c162422a9c2b25ca8bd14cf9
Olvisgil <48240668+Olvisgil@users.noreply.github.com>
2022-12-08 17:33:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
- <!--
         <ul className="w-full">
           {posts.map((post) => (
             <li
@@ -46,7 +45,6 @@ export default function Index({ posts, globalData }) {
             </li>
           ))}
         </ul>
-        -->
     </div>
   )
 }
